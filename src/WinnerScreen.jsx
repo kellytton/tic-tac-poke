a8e74b8d90fb2368ca774b33c42667e6bff99654
Kelly Ton <kthton@gmail.com>
2025-03-31 12:06:18 -0400

    congratulations animation

--- src/WinnerScreen.jsx
+++ src/WinnerScreen.jsx
@@ -9,7 +9,29 @@ function WinnerScreen({ winner, onReset, onQuit }) {
             <div className="winner">
                 <img className="winner-ball" src={winnerBall} alt="Winner's Pokeball" />
             </div>
-            <h1 className="winner-title">Congratulations !!</h1>
+            {/* <h1 className="winner-title">
+                Congratulations !!
+            </h1> */}
+            <h1 className="winner-title">
+                <span className="title-item">C</span>
+                <span className="title-item">o</span>
+                <span className="title-item">n</span>
+                <span className="title-item">g</span>
+                <span className="title-item">r</span>
+                <span className="title-item">a</span>
+                <span className="title-item">t</span>
+                <span className="title-item">u</span>
+                <span className="title-item">l</span>
+                <span className="title-item">a</span>
+                <span className="title-item">t</span>
+                <span className="title-item">i</span>
+                <span className="title-item">o</span>
+                <span className="title-item">n</span>
+                <span className="title-item">s</span>
+                <span className="title-item"> </span>
+                <span className="title-item">!</span>
+                <span className="title-item">!</span>
+            </h1>
             <div className="endgame-button">
                 <button className="button winnerscreen-button" onClick={onReset}>Play Again</button>
                 <button className="button winnerscreen-button" onClick={onQuit}>Home</button>
